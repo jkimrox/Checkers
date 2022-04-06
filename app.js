f6cb6cdb8b3af0d73868375ba6d04bcd7f3546bb
Jonathan <jonathankimwylie@gmail.com>
2022-04-06 14:15:22 -0500

    fixed pseudocode

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ Create the board -
 Game Start -
 
     Turns -
-        Players who have black pieces move first
-
+        Players who have black pieces move first -
         Moving -
         Pieces can only move diagonally across the board and single pieces are limited to forward moves
             is there an opponent piece adjacent diagonally to your piece?
@@ -20,7 +19,16 @@ Game Start -
             forward AND backward movement are available
             if a piece is available to jump, you are able to jump in several directions
 
-        Player who has red pieces goes after
+        Player who has red pieces goes after -
+        Moving -
+        Pieces can only move diagonally across the board and single pieces are limited to forward moves
+            is there an opponent piece adjacent diagonally to your piece?
+                if so, you must jump over
+                is there another piece available to be captured?
+                    if so, you can decide whether or not to capture it
+        Reaching the end of the board -> Upgrade to a King
+            forward AND backward movement are available
+            if a piece is available to jump, you are able to jump in several directions
     
 Game Finish -
     No pieces left on the board 
